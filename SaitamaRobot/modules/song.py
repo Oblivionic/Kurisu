a155b72d5f0b8d8a88810bca7de6f2ff09c524cf
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-04-06 13:13:57 +0530

    Update song.py

--- SaitamaRobot/modules/song.py
+++ SaitamaRobot/modules/song.py
@@ -16,7 +16,7 @@ except:
 	from youtubesearchpython import SearchVideos 
 	pass
 
-@register(pattern="^/song (.*)")
+#@register(pattern="^/song (.*)")
 async def download_video(v_url):
 
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
